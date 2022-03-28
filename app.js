368b43db307c57f9b5cf914f637957c5638e9a01
Daewony <96184691+Daewony@users.noreply.github.com>
2022-03-28 17:35:12 +0900

    Add files via upload

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-import {
-    Hill
-} from './hill.js';
+import {Hill} from './hill.js';
+import {SheepController} from './sheep-controller.js';
+import {Sun} from './sun.js';
 
 class App {
     constructor() {
@@ -9,16 +9,23 @@ class App {
         this.ctx = this.canvas.getContext('2d');
         document.body.appendChild(this.canvas);
 
+        this.sun = new Sun();
+
+        // 힐 추가하기
+        // 3D 효과 = (속도 기능)앞의 언덕은 빠르게, 뒤 언덕은 느리게 
+        // 촘촘한 언덕 = 좌표 추가하기
         this.hills = [
             new Hill('#fd6bea', 0.2, 12),
             new Hill('#ff59c2', 0.5, 8),
             new Hill('#ff4674', 1.4, 6)
         ];
 
-        // 스크린 사이즈를 가져오기 위한 리사이즈 이벤트 걸음
+        this.sheepController = new SheepController();
+        // 스크린 사이즈를 가져오기 위한 리사이즈 이벤트
         window.addEventListener('resize', this.resize.bind(this), false);
         this.resize();
-
+        
+        // 
         requestAnimationFrame(this.animate.bind(this));
     }
 
@@ -29,26 +36,35 @@ class App {
         // 캔버스 사이즈 2배, 선명하게 보이기 위함
         this.canvas.width = this.stageWidth * 2;
         this.canvas.height = this.stageHeight * 2;
-        this.ctx.scale(2,2);
+        this.ctx.scale(2, 2);
 
+        this.sun.resize(this.stageWidth, this.stageHeight);
+
+        // 언덕 랜덤으로 그리기
         for (let i=0; i<this.hills.length; i++){
             this.hills[i].resize(this.stageWidth,this.stageHeight);
         }
+
+        this.sheepController.resize(this.stageWidth, this.stageHeight);
     }
 
     animate(t) {
-        requestAnimationFrame(this.animate,bind(this));
+        requestAnimationFrame(this.animate.bind(this));
 
-        // 잔재 남는거 없앰
-        this.ctx.clearRect(0,0,this.stageWidth,this.stageHeight);
+        // 애니메이션 잔재 남는거 지우기
+        this.ctx.clearRect(0, 0, this.stageWidth, this.stageHeight);
+
+        this.sun.draw(this.ctx, t);
 
         let dots;
         for(let i=0; i<this.hills.length;i++){
             dots = this.hills[i].draw(this.ctx);
         }
+
+        this.sheepController.draw(this.ctx, t, dots);
     }
 }
 
 window.onload = () => {
     new App();
-}
+};
--- hill.js
+++ hill.js
@@ -1,4 +1,5 @@
 export class Hill {
+    // 언덕의 색상, 속도, 언덕의 포인드 개수
     constructor(color, speed, total) {
         this.color = color;
         this.speed = speed;
@@ -10,12 +11,14 @@ export class Hill {
         this.stageHeight = stageHeight;
 
         this.points = [];
+        // 각 좌표의 X값을 this.total 개수만큼 띄어줄 것임, 스테이지보다 크게 잡기위한 -2
+        // 화면 바깥으로부터 자연스럽게 들어오는 양의 모션을 위해 -2
         this.gap = Math.ceil(this.stageWidth / (this.total - 2));
 
         for (let i = 0; i < this.total; i++) {
             this.points[i] = {
                 x: i * this.gap,
-                y: this.getY()
+                y: this.getY() // Y값 랜덤으로 정의
             };
         }
     }
@@ -26,34 +29,38 @@ export class Hill {
 
         let cur = this.points[0];
         let prev = cur;
-
-        let dots =[];
+        // 양의 좌표를 찾기 위한 배열
+        let dots = [];
         // 오른쪽으로 이동 및 스피드 추가
         cur.x += this.speed;
 
         // 끊김없이 언덕 나오기, 일정영역에서 사라지면 배열에서 빼주고 새로운것을 앞으로 등 관리하기
         // 멀리있는 물체보다 가까이 있는 물체가 빠르게 지나가 애니메이션 효과 발생
         if (cur.x > -this.gap) {
+            // 앞에서 부터 추가
             this.points.unshift({
                 x: -(this.gap * 2),
                 y: this.getY()
             });
         } else if ( cur.x > this.stageWidth + this.gap) {
             this.points.splice(-1);
+            console.log(this.points);
         }
 
-        ctx.moveTo(cur.x,cur.y);
+        ctx.moveTo(cur.x, cur.y);
 
         let prevCx = cur.x;
         let prevCy = cur.y;
 
         for (let i=1; i<this.points.length;i++){
+            // 포인트 배열 가져오기
             cur = this.points[i];
             cur.x += this.speed;
             const cx = (prev.x + cur.x) / 2;
             const cy = (prev.y + cur.y) / 2;
+            // 곡선 그리기
             ctx.quadraticCurveTo(prev.x,prev.y,cx,cy);
-
+            // 리턴값
             dots.push({
                 x1: prevCx,
                 y1: prevCy,
@@ -65,7 +72,7 @@ export class Hill {
 
             prev = cur;
             prevCx = cx;
-            prev = cy;
+            prevCy = cy;
         }
 
         ctx.lineTo(prev.x,prev.y);
@@ -75,7 +82,7 @@ export class Hill {
 
         return dots;
     }
-
+    // 언덕의 Y값 랜덤으로 주기
     getY() {
         const min = this.stageHeight / 8;
         const max = this.stageHeight - min;
